f9c512ac45bb48265c788202274a0e836f4af7e2
CHEN YORONG <d994421040@gmail.com>
2021-08-06 15:40:28 +0800

    include

---
+++ test/0804-08-include.php
@@ -0,0 +1,14 @@
+<?php
+
+//如果找不到資源時 發出 error 較強烈
+require  __DIR__. '/0802-08-query-string.php';    //包含某個php檔案
+
+//如果找不到資源時 發出 warming
+include __DIR__. '/0802-08-query-string.php';    //包含某個php檔案
+
+
+
+
+
+
+?>
