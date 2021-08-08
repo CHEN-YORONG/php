469a2e796ddac03ccc738d7629837ea5dd742a20
CHEN YORONG <d994421040@gmail.com>
2021-08-08 18:13:29 +0800

    login-api :判斷密碼

--- proj/login-api.php
+++ proj/login-api.php
@@ -24,11 +24,29 @@ $output=[
 //$_POST['account']
 if(! isset($users[$_POST['account']])){
     $output['error']='帳號錯誤';
-    $output['code']='401';
+    $output['code']=401;
     echo json_encode($output,JSON_UNESCAPED_UNICODE);
     exit; //直接離開 (中斷)
     //die(); //換EXIT一樣
 }
+
+$userData = $users[$_POST['account']];
+if($_POST['password'] !== $userData['pw']){
+    $output['error']='密碼錯誤';
+    $output['code']='405';
+}else{
+    $output['success']=true;
+    $output['code']=200;
+
+    $_SESSION['user']=[
+        'account' => $_POST['account'],
+        'nickname' => $userData['nickname'],
+    ];
+
+}
+
+
+
 
 echo json_encode($output,JSON_UNESCAPED_UNICODE);
 
