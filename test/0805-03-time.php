1858cd22c8d57d6d5b76ee82ef62ef80986d7147
CHEN YORONG <d994421040@gmail.com>
2021-08-08 15:24:21 +0800

    strtotime

--- test/0805-03-time.php
+++ test/0805-03-time.php
@@ -1,7 +1,14 @@
 <h2>
     <?php 
+    // https://www.php.net/manual/zh/function.date.php
+    date_default_timezone_set('Asia/Taipei'); //預設是德國時區 修改為台北時區
     echo time().'<br>';
-    echo date("Y-m-d H:i:s");
+    echo date("Y-m-d H:i:s").'<br>';
+    echo date("D N").'<br>';
+    echo date("Y-m-d H:i:s",time()+40*24*60*60); //40天後
     //格式化時間
+
+    $t = strtotime('2021/09/06').'<br>';
+    echo date("Y-m-d H:i:s",$t).'<br>';
     ?>
 </h2>
