708794eed77270150d6b9241808c5a884a69afa4
CHEN YORONG <d994421040@gmail.com>
2021-08-10 22:02:32 +0800

    新增資料 2.1

--- proj/data_insert.php
+++ proj/data_insert.php
@@ -7,33 +7,31 @@ $title = '新增資料';
 <div class="container">
     <div class="row">
         <div class="col-md-6">
-
-            <div class="card" >
-
+            <div class="card">
                 <div class="card-body">
                     <h5 class="card-title">新增資料</h5>
-                    <form name="form1" onsubmit="checkForm();return false;">
+                    <form name="form1" >
                         <div class="form-group">
                             <label for="name">name</label>
-                            <input type="text" class="form-control" id="name" >
+                            <input type="text" class="form-control" id="name" name="name" >
                             <small  class="form-text text-muted"></small>
                         </div>
 
                         <div class="form-group">
                             <label for="email">email</label>
-                            <input type="text" class="form-control" id="email" >
+                            <input type="text" class="form-control" id="email" name="email">
                             <small  class="form-text text-muted"></small>
                         </div>
 
                         <div class="form-group">
                             <label for="mobile">mobile</label>
-                            <input type="text" class="form-control" id="mobile" >
+                            <input type="text" class="form-control" id="mobile" name="mobile">
                             <small  class="form-text text-muted"></small>
                         </div>
 
                         <div class="form-group">
                             <label for="bithday">bithday</label>
-                            <input type="text" class="form-control" id="bithday" >
+                            <input type="text" class="form-control" id="bithday" name="bithday">
                             <small  class="form-text text-muted"></small>
                         </div>
                         
@@ -52,7 +50,7 @@ $title = '新增資料';
 <script>
 function checkForm(){
 
-    
+
 }
 
 </script>
