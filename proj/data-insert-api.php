107dec643d82eb4e92d61bd43671a3c429d47429
CHEN YORONG <d994421040@gmail.com>
2021-08-10 22:46:16 +0800

    新增表單 5 錯誤的做法

--- proj/data-insert-api.php
+++ proj/data-insert-api.php
@@ -1,5 +1,20 @@
 <?php 
-echo json_encode($_POST);
+include __DIR__ .'/partoals/init.php';
+// echo json_encode($_POST);
 
+
+//錯誤的做法
+$sql="INSERT INTO `address_book`(
+    `name`, `email`, `mobile`, `bithday`, `created_at`) 
+    VALUES(
+        '{$_POST['name']}','{$_POST['email']}','{$_POST['mobile']}','{$_POST['bithday']}',now()
+    )";
+
+$stmt = $pdo->query($sql);
+
+echo json_encode([
+    'rowCount' => $stmt->rowCount(), //新增的筆數
+    'postData' => $_POST,
+]);
 ?>
 
