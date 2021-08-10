b349e6a8ab4af13da28c5d2034874572100df422
CHEN YORONG <d994421040@gmail.com>
2021-08-10 18:02:39 +0800

    分頁按鈕

--- proj/data_list.php
+++ proj/data_list.php
@@ -8,7 +8,7 @@ $perPage = 5;
 
 
 //用戶決定查看第幾頁
-$page = isset($_GET['page']) ? intval($_GET['page']) :1 ;   //intval(轉換成整數)
+$page = isset($_GET['page']) ? intval($_GET['page']) : 1;   //intval(轉換成整數)
 
 //總共有幾筆
 $totalRows = $pdo->query("SELECT count(1) FROM address_book")->fetch(PDO::FETCH_NUM)[0];
@@ -17,7 +17,7 @@ $totalRows = $pdo->query("SELECT count(1) FROM address_book")->fetch(PDO::FETCH_
 
 //總共有幾頁才能生出分業按鈕
 
-$totalPages = ceil($totalRows/$perPage); //ceil() 無條件進位
+$totalPages = ceil($totalRows / $perPage); //ceil() 無條件進位
 // echo "$totalRows,$totalPages"; exit; 查看幾筆,幾頁
 
 
@@ -25,7 +25,7 @@ $totalPages = ceil($totalRows/$perPage); //ceil() 無條件進位
 //SELECT * FROM address_book ORDER BY sid DESC LIMIT 5,5  第二頁
 //($page-1)*$perPage  第二頁 (2-1)*5=5   第三頁 (3-1)*5=10
 
-$sql = sprintf("SELECT * FROM address_book ORDER BY sid DESC LIMIT %s,%s", ($page-1)*$perPage , $perPage );
+$sql = sprintf("SELECT * FROM address_book ORDER BY sid DESC LIMIT %s,%s", ($page - 1) * $perPage, $perPage);
 
 
 $row = $pdo->query($sql) //ORDER BY sid DESC 從後面排回來8 .7 .6 最新的資料開始排
@@ -39,6 +39,21 @@ $row = $pdo->query($sql) //ORDER BY sid DESC 從後面排回來8 .7 .6 最新的
 <?php include __DIR__ . '/partoals/html-head.php'; ?>
 <?php include __DIR__ . '/partoals/navbar.php'; ?>
 <div class="container">
+    <div class="row">
+        <div class="col">
+            <nav aria-label="Page navigation example">
+                <ul class="pagination d-flex justify-content-end">
+                    <!-- <li class="page-item"><a class="page-link" href="#">Previous</a></li> -->
+                    <?php for ($i=1;$i<=$totalPages;$i++): ?>
+                    <li class="page-item"><a class="page-link" href="?page=<?= $i ?>"><?= $i ?></a></li>
+                    <?php endfor; ?>
+                    <!-- <li class="page-item"><a class="page-link" href="#">Next</a></li> --> 
+                </ul>
+            </nav>
+        </div>
+    </div>
+
+
     <div class="row">
         <div class="col">
             <table class="table table-striped table-bordered">
@@ -67,11 +82,11 @@ $row = $pdo->query($sql) //ORDER BY sid DESC 從後面排回來8 .7 .6 最新的
         </div>
 
         <!-- <div>
-            <?php foreach($row as $r):?>
+            <?php foreach ($row as $r) : ?>
                 <h2><?= $r['name'] ?></h2>
             <?php endforeach; ?>
         </div> -->
-        
+
 
     </div>
 </div>
