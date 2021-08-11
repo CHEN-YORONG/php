6261af9bc14a72c742a3d7f277872d8480ab99ff
CHEN YORONG <d994421040@gmail.com>
2021-08-11 16:27:02 +0800

    分頁太多 縮成五個

--- proj/data_list.php
+++ proj/data_list.php
@@ -71,10 +71,15 @@ $row = $pdo->query($sql) //ORDER BY sid DESC 從後面排回來8 .7 .6 最新的
 
 
 
-                    <?php for ($i = 1; $i <= $totalPages; $i++) : ?>
+                    <?php for ($i = $page-5; $i <= $page+5; $i++) : 
+                          if($i>=1 and $i<=$totalPages):
+                        ?>
+
                         <li class="page-item <?= $i == $page ? 'active' : '' ?>"><a class="page-link" href="?page=<?= $i ?>"><?= $i ?></a></li>
                         <!-- active b4屬性 反白 -->
-                    <?php endfor; ?>
+                    <?php 
+                    endif;
+                    endfor; ?>
 
                     <!-- 下一頁 -->
                     <li class="page-item <?= $page >= $totalPages ? 'disabled' : ''  ?>">
