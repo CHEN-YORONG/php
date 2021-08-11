f8f824f8c4f5cd5d63f9f45258dbb5f1f9ec2557
CHEN YORONG <d994421040@gmail.com>
2021-08-11 10:45:42 +0800

    新增資料表單 8: 前端

--- proj/data_insert.php
+++ proj/data_insert.php
@@ -4,6 +4,16 @@ $title = '新增資料';
 ?>
 <?php include __DIR__ . '/partoals/html-head.php'; ?>
 <?php include __DIR__ . '/partoals/navbar.php'; ?>
+    <style>
+        form .form-group small{
+            color: red;
+        }
+    </style>
+
+
+
+
+
 <div class="container">
     <div class="row">
         <div class="col-md-6">
@@ -12,27 +22,27 @@ $title = '新增資料';
                     <h5 class="card-title">新增資料</h5>
                     <form name="form1" onsubmit="checkForm();return false;">
                         <div class="form-group">
-                            <label for="name">name</label>
-                            <input type="text" class="form-control" id="name" name="name" >
-                            <small  class="form-text text-muted"></small>
+                            <label for="name">name *</label>
+                            <input type="text" class="form-control" id="name" name="name">
+                            <small  class="form-text ">hj</small>
                         </div>
 
                         <div class="form-group">
-                            <label for="email">email</label>
-                            <input type="text" class="form-control" id="email" name="email">
-                            <small  class="form-text text-muted"></small>
+                            <label for="email">email *</label>
+                            <input type="text" class="form-control" id="email" name="email" >
+                            <small  class="form-text "></small>
                         </div>
 
                         <div class="form-group">
                             <label for="mobile">mobile</label>
                             <input type="text" class="form-control" id="mobile" name="mobile">
-                            <small  class="form-text text-muted"></small>
+                            <small  class="form-text "></small>
                         </div>
 
                         <div class="form-group">
                             <label for="bithday">bithday</label>
                             <input type="text" class="form-control" id="bithday" name="bithday">
-                            <small  class="form-text text-muted"></small>
+                            <small  class="form-text "></small>
                         </div>
                         
                         <button type="submit" class="btn btn-primary">新增</button>
@@ -48,9 +58,18 @@ $title = '新增資料';
 <?php include __DIR__ . '/partoals/scripts.php'; ?>
 
 <script>
-function checkForm(){
-    //TODO: 資料欄位檢查
 
+    //html5作法 <input   require 必填 >
+const email_re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+const mobile_re = /^09\d{2}-?\d{3}-?\d{3}-?/;
+
+
+function checkForm(){
+    //TODO:欄位的外觀要回復成原來的狀態
+    
+    //TODO: 資料欄位檢查
+    //TODO:如果格式不符 要有欄位提示的不同外觀
 
     const fd =new FormData(document.form1);
     fetch('data-insert-api.php',{
